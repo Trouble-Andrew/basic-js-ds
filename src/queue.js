bb87a724fe81f95e532f2d9ac9be02179c6fc1d1
Trouble-Andrew <satyr663@gmail.com>
2022-04-28 15:46:11 +0300

    feat: implement queue ds

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
+const {ListNode} = require('../extensions/list-node.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,22 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    let next = this.head;
+    if (this.length === 0) {
+      this.head = new ListNode(value);
+    } else {
+      while (next.next) {
+        next = next.next;
+      }
+      next.next = new ListNode(value);
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const outElement = this.head;
+
+    this.head = outElement.next;
+    this.length > 0 ? this.length-- : '';
+
+    return outElement.value;
   }
 }
+
 
 module.exports = {
   Queue
